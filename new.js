caea62dd3d717129ac5a43467a1b8fd15a0be386
tariktesfa <tariktesfa9090@gmail.com>
2021-09-17 17:05:08 +0300

    New files added

--- new.js
+++ new.js
@@ -1,6 +1,19 @@
-alert("Hi there!");
-var Name= "My name";
-var is= "is";
-var firstName="tarik";
-
-console.log(Name + is + firstName);
+/******************************************************************
+ ******************Coding Challenge 1******************************
+ *********************BMI Calculator********************************/
+ var johnMass, markMass, johnHeight, markHeight, BMIofMark, BMIofJohn ;
+ //John's BMI
+ johnMass = prompt('Please enter John Masss in Kg');
+ johnHeight = prompt('Enter John height in meter');
+ //BMI calculation
+ BMIofJohn = johnMass/(johnHeight * johnHeight);
+ console.log('BMI'+' '+'of John is '+ BMIofJohn);
+ //Marks's BMI
+ markMass = prompt('Please enter Mark Masss in Kg');
+ markHeight = prompt('Enter Mark height in meter');
+ //BMI calculation
+ BMIofMark = markMass/(markHeight * markHeight);
+ console.log('BMI'+' '+'of Mark is '+ BMIofMark);
+ //Checking thier BMI
+ var isGreater = BMIofJohn > BMIofMark;
+ console.log("Is John's BMI higher than Mark's? " + isGreater);
